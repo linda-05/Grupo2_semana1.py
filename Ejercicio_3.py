8acdb7054ea0a26bcc1830f72129568054c3cce8
linda-05 <aa0531032025@unab.edu.sv>
2025-07-28 19:26:40 -0600

    declarando numero para realizar una suma, resta, multiplicacion divion y saber cual es el numero mayor

---
+++ Ejercicio_3.py
@@ -0,0 +1,21 @@
+# declarando numeros
+numero_1 = 8
+numero_2 = 2
+
+# suma
+print("La suma de", numero_1, "y", numero_2, "es:", numero_1 + numero_2)
+
+# diferencia
+print("La diferencia de", numero_1, "y", numero_2, "es:", numero_1 - numero_2)
+
+# prducto
+print("El producto de", numero_1, "y", numero_2, "es:", numero_1 * numero_2)
+
+#division
+print("La division de", numero_1, "y", numero_2, "es:", numero_1 // numero_2)
+
+# numero mayor
+if numero_1 > numero_2:
+    print("El numero mayor es:", numero_1)
+elif numero_2 > numero_1: 
+    print("El numero mayor es:", numero_2)
