924da529d59eafea4769529c0a96e1c40592b73b
linda-05 <aa0531032025@unab.edu.sv>
2025-07-28 19:22:18 -0600

    solicitando al usuario un numero para multiplicarlo por dos

---
+++ Ejercicio_2.py
@@ -0,0 +1,11 @@
+# solicitar un numero al usuario
+numero = int(input("Ingrese un numero:"))
+
+# convertir la entrada a entero
+numero_entero = int(numero)
+
+# multiplicar el numero por 2
+resultado = numero_entero * 2
+
+# mostrar el resultado
+print("El resultado de multplicar", numero_entero, "por 2 es:", resultado)
